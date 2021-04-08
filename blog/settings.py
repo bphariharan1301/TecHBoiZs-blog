8813fe930dd3601d5175bffa86774e75c0492107
HARIHARAN B P <bphariharan1301@gmail.com>
2021-04-08 13:22:30 +0530

    Edit Settings

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['techboizsblog.herokuapp.com', '127.0.0.1']
 
