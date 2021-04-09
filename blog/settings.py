3435e0cf182c5a49a41ba26a1f6585c662c93cd8
HARIHARAN B P <bphariharan1301@gmail.com>
2021-04-09 16:25:39 +0530

    Added files and changes required for deployment to heroku

--- blog/settings.py
+++ blog/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
+# DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['techboizsblog.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['techboizsblog.herokuapp.com', '127.0.0.1', 'techboizs-blog.herokuapp.com']
 
 # ALLOWED_HOSTS = []
 
